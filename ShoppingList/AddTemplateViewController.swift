a18399b87dec4c037063052763ece46ec307373f
SCHATZMANN Julia <jsc3127@d003-08.lan.uclv.net>
2018-04-26 14:32:36 +0200

    add new Temples

---
+++ ShoppingList/AddTemplateViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddTemplateViewController.swift
+//  ShoppingList
+//
+//  Created by SCHATZMANN Julia on 26.04.18.
+//  Copyright © 2018 Timo Rzipa. All rights reserved.
+//
+
+import UIKit
+
+class AddTemplateViewController: UIViewController {
+
+    @IBOutlet weak var cancelBtn: UIBarButtonItem!
+    @IBOutlet weak var saveBtn: UIBarButtonItem!
+    @IBOutlet weak var templateName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ ShoppingList/TemplateTableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  TemplateTableViewController.swift
+//  ShoppingList
+//
+//  Created by SCHATZMANN Julia on 26.04.18.
+//  Copyright © 2018 Timo Rzipa. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TemplateTableViewController: UITableViewController {
+
+    var templates: [Template] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Templates"
+        
+        // get app delegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        // get managed context
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // request
+        let fetchRequest = NSFetchRequest<Template>(entityName: "Template")
+        // load data
+        do {
+            templates = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let destination = storyboard.instantiateViewController(withIdentifier: "TemplateItemController") as! TemplateTableViewController
+        let template = templates[indexPath.row];
+        
+        destination.templateId = (template.value(forKeyPath: "id") as? Int16)!
+        
+        destination.templateName = (template.value(forKeyPath: "name") as? String)!
+        
+        navigationController?.pushViewController(destination, animated: true)
+        
+    }
+    */
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return templates.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let template = templates[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "templateCell", for: indexPath)
+        cell.textLabel?.text = template.value(forKeyPath: "name") as? String
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            let managedContext = appDelegate.persistentContainer.viewContext
+            let template = templates[indexPath.row]
+            managedContext.delete(template);
+            templates.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+    }
+    */
+    
+    
+    @IBAction func saveTemplate(_ segue:UIStoryboardSegue){
+        if let svc = segue.source as? AddTemplateViewController {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            let managedContext = appDelegate.persistentContainer.viewContext
+            
+            var max : Int16 = 0;
+            
+            for template in templates{
+                if(template.id > max){
+                    max = template.id
+                }
+            }
+            max = max + 1
+            
+            let template = NSEntityDescription.insertNewObject(forEntityName: "Template", into: managedContext) as! Template
+            template.name = svc.templateName.text
+            template.id = max;
+            do {
+                try managedContext.save()
+                templates.append(template)
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+            
+            self.tableView.reloadData()
+        }
+    }
+    
+    @IBAction func cancelAddTemplate(_ segue:UIStoryboardSegue){
+    }
+}
